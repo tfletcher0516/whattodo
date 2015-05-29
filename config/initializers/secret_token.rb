75d723c78727849e7c777d6ec6e7f9fc7d16a52c
Tim Fletcher <fletcher0516@gmail.com>
2015-05-29 04:07:31 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whattodo::Application.config.secret_key_base = '6af5a95885239ebaf654ac6ba5b240e3512c5f2b2a4b3a01f1df713ea4cc591dd38ad46a0c2a09d6c737db7f7e60cc543a77d1a62c9657875277e00b21749772'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whattodo::Application.config.session_store :cookie_store, key: '_whattodo_session'
